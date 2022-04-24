fdf7b52d65f67fab839a9b82c41cac188336a774
Guilherme <guilhermeevencioluz@gmail.com>
2022-04-24 16:58:51 -0300

    feat: Cria o app react, adiciona as rotas e cria as páginas para as suas respectivas rotas.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Wallet from './Pages/Wallet';
+import NotFound from './Pages/NotFound';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>MyWallet</h1>
+       <Routes>
+         <Route exact path="/" element={ <Home /> } />
+         <Route exact path="/wallet" element={ <Wallet /> } />
+         <Route path="*" element={ NotFound } />
+       </Routes>
+    </div>
+  );
+}
+
+export default App;
