b2bd045ad00173c211e553b93cebbd36fff64a5a
Guilherme <guilhermeevencioluz@gmail.com>
2022-04-24 20:00:20 -0300

    feat: Implementa a validação do formulario na página de login.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,57 @@
-import React from 'react';
-// import { useNavigate } from 'react-router-dom'
+import { isDisabled } from '@testing-library/user-event/dist/utils';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+  const [userName, setUserName] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [isButtonDisabled, setIsDisabled] = useState(true);
+  const history = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    history('/wallet')
+  }
+
+  const handleChange = ({ target }) => {
+    switch (target.id) {
+      case 'user_input':
+        setUserName(target.value);
+        break;
+      case 'password_input':
+        (target.value.length > 5) ? setIsDisabled(false) : setIsDisabled(true)
+        setUserPassword(target.value);
+        break;
+    
+      default:
+        break;
+    }
+  }
+
   return (
     <div id="home_container">
       <h1 id="home_title">MyWallet</h1>
       <input
-        type="email"
-        id="email_input"
-        placeholder="Email"
+        type="text"
+        id="user_input"
+        placeholder="Usuário"
+        onChange={ handleChange }
       />
       <input
         type="password"
         id="password_input"
         placeholder="Senha"
+        onChange={ handleChange }
       />
+
+      <button
+        type="submit"
+        onClick={ handleSubmit }
+        disabled={ isButtonDisabled }
+      >
+        Entrar
+      </button>
+
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { configureStore  } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux'
+
+// export const store = configureStore({
+//   // reducer
+// })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    {/* <Provider store={ store }> */}
+      <Router>
+        <App />
+      </Router>
+    {/* </Provider> */}
   </React.StrictMode>
 );
 
