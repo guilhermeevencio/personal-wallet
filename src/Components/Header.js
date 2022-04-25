b2c90b6792f0e59521304c6e1ff38fc8eded0fea
Guilherme <guilhermeevencioluz@gmail.com>
2022-04-25 14:58:34 -0300

    feat: Adiciona a soma dos valore da despesa no componente Header.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,11 +3,14 @@ import { useSelector } from 'react-redux';
 
 const Header = () => {
   const userName = useSelector(({ userInfo: { user } }) => user.userName);
+  const expenseValue = useSelector(({ savedExpenses: { expenses } }) => expenses);
+  const expensesValueSum = expenseValue
+    .reduce((acc, {value, currency, exchangeRates }) => acc + (Number(value) * Number(exchangeRates[currency].ask)), 0);
   
   return (
     <div>
       <h3>{`Olá, ${ userName }!`}</h3>
-      <h3>Total: 0 BRL</h3>
+      <h3>{`Total: ${ expensesValueSum.toFixed(2) } BRL`}</h3>
     </div>
   )
 }
--- src/Pages/Wallet.js
+++ src/Pages/Wallet.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import ExpenseForm from '../Components/ExpenseForm';
 import Header from '../Components/Header';
 import { fetchCurrencies } from '../features/saveCurrenciesCode';
 
 const Wallet = () => {
+  const [isFormEnabled, setIsFormEnabled] = useState(false)
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchCurrencies());
@@ -12,7 +13,15 @@ const Wallet = () => {
   return (
     <div>
       <Header />
-      <ExpenseForm />
+      {isFormEnabled
+        && <ExpenseForm />
+      }
+      <button
+      type="button"
+      onClick={ () => setIsFormEnabled(!isFormEnabled) }
+      >
+        +
+      </button>
     </div>
   )
 }
