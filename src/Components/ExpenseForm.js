eaed5c0ee010c751b3a04a0304a73b8fb7e573d5
Guilherme <guilhermeevencioluz@gmail.com>
2022-04-25 09:57:41 -0300

    feat: Adiciona os dados do formulário ao estado do componente ExpenseForm.

--- src/Components/ExpenseForm.js
+++ src/Components/ExpenseForm.js
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const ExpenseForm = () => {
+  const currencies = useSelector((state) => state.currenciesCodeSlice.currencies)
+  const [expense, setExpense] = useState({
+    currency: 'USD',
+    description: '',
+    method: 'Dinheiro',
+    tag: 'Alimentação',
+    value: "0",
+  });
+
+  const handleChange = ({ target }) => {
+    setExpense({
+      ...expense, 
+      [target.name]: target.value,
+    })
+  }
+
+   const handleSubmit = (event) => {
+     event.preventDefault();
+     console.log(expense);
+   } 
   return (
     <div>
       <form>
         <input
           type="number"
+          name="value"
           id="value-input"
+          onChange={ handleChange }
+          defaultValue="0"
         />
         <select
           id="currency-input"
+          name="currency"
+          onClick={ handleChange }
         >
-
+          { currencies.length !== 0
+            && (currencies.map((currency, index) => (<option key={ index }>{currency}</option>)))
+          }
         </select>
         <select
           id="method-input"
+          name="method"
+          onClick={ handleChange }
         >
           <option>Dinheiro</option>
           <option>Cartão de crédito</option>
@@ -22,14 +52,29 @@ const ExpenseForm = () => {
         </select>
         <select
           id="tag-input"
+          name="tag"
+          defaultValue="Selecione a Categoria"
+          onClick={ handleChange }
         >
-          <option>Alimentaão</option>
+          <option>Alimentação</option>
           <option>Lazer</option>
           <option>Trabalho</option>
           <option>Transporte</option>
           <option>Saúde</option>
         </select>
-        <input />
+        <input
+          type="text"
+          name="description"
+          id="description-input"
+          onChange={ handleChange }
+          placeholder="Descrição"
+        />
+        <button
+        id="submit-expense-button"
+        onClick={ handleSubmit }
+        >
+          Adicionar Despesa
+        </button>
       </form>
     </div>
   )
